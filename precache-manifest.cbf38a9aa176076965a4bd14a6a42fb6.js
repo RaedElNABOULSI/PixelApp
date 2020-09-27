d8e4e8fd7ad575693c099f528943251dad13908a
raed <testuser@example.com>
2020-09-27 13:38:02 +0300

    Updates

---
+++ precache-manifest.cbf38a9aa176076965a4bd14a6a42fb6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac69d2ab53db39f91fa6310a5afd2cf",
+    "url": "/PixelApp/index.html"
+  },
+  {
+    "revision": "7b0dc56e6fab625b927d",
+    "url": "/PixelApp/static/css/2.4abefa25.chunk.css"
+  },
+  {
+    "revision": "bd4a04f51560e5c05f93",
+    "url": "/PixelApp/static/css/main.ac454218.chunk.css"
+  },
+  {
+    "revision": "7b0dc56e6fab625b927d",
+    "url": "/PixelApp/static/js/2.60c4e14a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PixelApp/static/js/2.60c4e14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4a04f51560e5c05f93",
+    "url": "/PixelApp/static/js/main.11256c94.chunk.js"
+  },
+  {
+    "revision": "05615c57c3bfae08f2bb",
+    "url": "/PixelApp/static/js/runtime-main.7cb8ca8d.js"
+  },
+  {
+    "revision": "337b78d60d264e33fd4e877815d43c89",
+    "url": "/PixelApp/static/media/desert.337b78d6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PixelApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PixelApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PixelApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PixelApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PixelApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "43100580a522c49d3eb6ba88164ac938",
+    "url": "/PixelApp/static/media/hero-sea.43100580.jpg"
+  },
+  {
+    "revision": "b3b9be7208bdf62fbdb788be821f89cb",
+    "url": "/PixelApp/static/media/logo.b3b9be72.jpg"
+  },
+  {
+    "revision": "5d8880c8a3281aa96d968553e09edbf1",
+    "url": "/PixelApp/static/media/park.5d8880c8.jpg"
+  },
+  {
+    "revision": "4e79f923f9b8a62a4f50177e9f5ca1c4",
+    "url": "/PixelApp/static/media/river.4e79f923.jpg"
+  }
+]);
